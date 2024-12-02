fe5238b09807b5c5078441d3e725975041460c5c
Nando717 <iverlandioedsonn@gmail.com>
2024-12-02 17:30:50 -0300

    adicionei a função get  na minha aplicação para listar todos meus produtos no BD

--- src/main/java/com/product/api/controllers/ProductController.java
+++ src/main/java/com/product/api/controllers/ProductController.java
@@ -9,12 +9,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/")
 
 public class ProductController {
 
@@ -22,14 +22,18 @@ public class ProductController {
     ProductRepository productRepository;
 
 
-
     @PostMapping("/products")
 
-    public ResponseEntity<ProductModel>saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
-    var productModel = new ProductModel();
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
 
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAll() {
+       return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
 
 }
