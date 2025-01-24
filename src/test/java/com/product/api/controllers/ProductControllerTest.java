fef0bb6ea16693c826923e9ade87a71fc7ecf590
Nando717 <iverlandioedsonn@gmail.com>
2025-01-24 18:10:54 -0300

    Iniciando testes Unitarios com Junit e Mockito

---
+++ src/test/java/com/product/api/controllers/ProductControllerTest.java
@@ -0,0 +1,12 @@
+package com.product.api.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void saveProduct() {
+    }
+}
