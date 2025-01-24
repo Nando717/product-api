2e2df0062d2c20a5fa1ac57d3f8a38745ca329e7
Nando717 <iverlandioedsonn@gmail.com>
2025-01-24 18:40:36 -0300

    --allow-empty

--- src/test/java/com/product/api/controllers/ProductControllerTest.java
+++ src/test/java/com/product/api/controllers/ProductControllerTest.java
@@ -1,9 +1,10 @@
 package com.product.api.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DataJpaTest
 class ProductControllerTest {
 
     @Test
